0f2557961d563147378220994554ede03eb7af89
Rashedul Hasan <Rashedul Hasan>
2022-05-09 23:47:26 +0600

    JS Class-01

---
+++ JS-Class-01/JS-Class-01.js
@@ -0,0 +1,50 @@
+// var mangoPrice = 10;
+// console.log(mangoPrice);
+
+
+// var mangoPrice = 10;
+// console.log(typeof mangoPrice);
+
+// var myName = "Tapu";
+// console.log(typeof myName);
+
+// var isGood = true;
+// console.log(typeof isGood);
+// var isBad = false;
+// console.log(typeof isBad)
+
+
+// var myCountry = "I love my country";
+// var toLowerCase = myCountry.toLowerCase();
+// console.log(toLowerCase);
+// var toUpperCase = myCountry.toUpperCase();
+// console.log(toUpperCase);
+// var position = myCountry.indexOf('love');
+// console.log(position);
+// var split = myCountry.split(" ");
+// console.log(split);
+
+// var num = '5';
+// var num2 = '7';
+// var total = num+num2;
+// console.log(total);
+
+// var num = parseInt('5');
+// var num2 = parseInt('7');
+// var total = num+num2;
+// console.log(total);
+
+// var num = 10;
+// var num2 = 3;
+// var add = num+num2; //10
+// var sub = num-num2; //7
+// var multi = num*num2; //30
+// var div = num/num2; // 3.33....
+// var rem = num%num2; // 1
+// console.log(add,sub,multi,div,rem);
+
+
+var firstName = "Rashedul";
+var lastName = "Hasan";
+var fullName = firstName+" "+lastName;
+console.log(fullName);
